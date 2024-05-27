14e280ddf40fbfa7b308eef3eed698f0f2316e65
Efrexz <zyzz_448@hotmail.com>
2024-05-27 08:18:07 -0500

    arreglando responsive de los TodoItems

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import { CheckButtonIcon } from "./CheckButtonIcon"
 import { DeleteButtonIcon } from "./DeleteButtonIcon"
 import { EditIcon } from "./EditIcon"
 
-function TodoItem({text,completed,checkTask, deleteTask}) {
+function TodoItem({ text, completed, checkTask, deleteTask }) {
 
 
     return (
@@ -11,21 +11,21 @@ function TodoItem({text,completed,checkTask, deleteTask}) {
             <div className="flex justify-around gap-3 items-center text-md">
                 <button
                     onClick={checkTask}
-                    className={`flex items-center justify-center w-6 h-6 rounded-full border ${completed ? "bg-green-200" : "bg-white"} border-gray-400 `}
+                    className={`flex items-center justify-center min-w-6 h-6 rounded-full border ${completed ? "bg-green-200" : "bg-white"} border-gray-400 `}
                 >
-                    <CheckButtonIcon completed={completed}/>
+                    <CheckButtonIcon completed={completed} />
                 </button>
                 <p className={completed ? "text-[#6C6C92] line-through" : "text-white"}>{text}</p>
             </div>
             <div className="flex items-center">
                 {/*Si la tarea esta completa no se agrega el boton de editar */
                     !completed &&
-                        <button className="absolute right-8 hover:cursor-pointer" title="Editar">
-                            <EditIcon text={text}/>
-                        </button>
+                    <button className="hover:cursor-pointer pr-4" title="Editar">
+                        <EditIcon text={text} />
+                    </button>
                 }
-                <button className="absolute right-[-12px] bottom-6 hover:cursor-pointer" title="Eliminar" onClick={deleteTask}>
-                    <DeleteButtonIcon/>
+                <button className="absolute right-[-12px] top-[-14px] hover:cursor-pointer" title="Eliminar" onClick={deleteTask}>
+                    <DeleteButtonIcon />
                 </button>
             </div>
         </li>
